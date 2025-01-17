cd7f227bbb611a1613b16d589806108fd58cb062
brobeldeep <97376434+brobeldeep@users.noreply.github.com>
2025-01-17 07:35:46 +0000

    upload data

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
         <button
           className="h-12 w-full max-w-md border-black border-2 p-2.5 bg-[#A6FAFF] hover:bg-[#79F7FF] hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-[#00E1EF]"
         >
-          Simple Button
+          Upload
         </button>
       </div>
     </div>
